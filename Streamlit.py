bb509697f1aed566c8eaad41c68a1a0e2b0d1d49
Sho Kawasaki <sho_kawasaki_gst@jp.honda>
2025-04-11 18:42:27 +0900

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -5,7 +5,7 @@ from matplotlib.patches import Patch
 import seaborn as sns
 
 # CSV読み込み
-df = pd.read_csv("nl_west_2022~2024_stats.csv", encoding="utf-8")
+df = pd.read_csv("nl_west_2022_2024_stats.csv", encoding="utf-8")
 
 # チームカラー定義
 team_colors = {
